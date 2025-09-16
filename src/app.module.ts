aaa8dad0f47303100394a8c45bb2296dcb14d7fe
vijaykapoor2001-sudo <vijaykapoor2001@gmail.com>
2025-09-16 20:49:20 +0530

    Fixed Signup and SignIn issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [AuthModule,  BookmarkModule, PrismaModule], 
+  imports: [ConfigModule.forRoot({isGlobal: true}), 
+    AuthModule,  BookmarkModule, PrismaModule], 
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,39 +2,30 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as argon2 from 'argon2';
-import { Prisma } from 'generated/prisma';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  // SIGNUP
-  async signup(dto: AuthDto) {
-    // hash password
+   async signup(dto: AuthDto) {
     const hash = await argon2.hash(dto.password);
+
     try {
-      // create user (exclude hash in response)
       const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           hash,
         },
-        //delete user.hash;
-        /*
-        select: {
-          id: true,
-          email: true,
-          createdAt: true, // add other safe fields if you want
-        },
-        */
       });
 
-      return user;
+      // exclude hash
+      const { hash: _removed, ...result } = user;
+      return result;
+
     } catch (error) {
-      if (error instanceof
-        Prisma.PrismaClientKnownRequestError
-      ) {
-        // if error is because of duplicate email
+      if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
           throw new ForbiddenException('Email already exists');
         }
@@ -43,11 +34,27 @@ export class AuthService {
     }
   }
 
-  // SIGNIN (for now just returns message)
-  signin() {
-    return { msg: 'I am signed in' };
-  }
 
+  // SIGNIN (for now just returns message)
+  async signin(dto: AuthDto) {
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    const pwMatches = await argon2.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    } 
+    // exclude hash before returning
+    const { hash, ...userWithoutHash } = user;
+    return userWithoutHash;
+  }
   // GET ALL USERS (safe fields only)
   async getUsers() {
     return this.prisma.user.findMany({
